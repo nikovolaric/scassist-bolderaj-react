11c4d463c7c9492e25b8d8994e63f9cc4cd4244a
nikovolaric <niko.volaric@gmail.com>
2025-06-28 17:38:11 +0200

    Initial commit

--- src/pages/User/MyClasses.tsx
+++ src/pages/User/MyClasses.tsx
@@ -1,13 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 import Header from "../../components/Header";
+import Spinner from "../../components/Spinner";
 import MyClassesInfo from "../../features/dashboard/classes/components/MyClassesInfo";
 import WelcomeSection from "../../features/dashboard/components/WelcomeSection";
+import { getMe } from "../../services/userAPI";
 
 function MyClasses() {
+  const { data, isPending } = useQuery({
+    queryKey: ["me"],
+    queryFn: getMe,
+  });
+
+  if (isPending) return <Spinner />;
+
   return (
     <div className="my-16 flex flex-col gap-20">
       <Header />
       <MyClassesInfo />
-      <WelcomeSection ticketsPage />
+      <WelcomeSection ticketsPage data={data} />
     </div>
   );
 }
