2eaee8fc16ec938d5f3f8ba5e2a91cf500a0f482
nikovolaric <niko.volaric@gmail.com>
2025-06-28 17:39:53 +0200

    Initial commit

--- src/pages/User/MyTickets.tsx
+++ src/pages/User/MyTickets.tsx
@@ -1,10 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
 import Header from "../../components/Header";
 import WelcomeSection from "../../features/dashboard/components/WelcomeSection";
 import MyValidTickets from "../../features/dashboard/visitsTicketsAndClasses/MyValidTickets";
 import MyVisits from "../../features/dashboard/visitsTicketsAndClasses/MyVisits";
 import MyVisitsThisYear from "../../features/dashboard/visitsTicketsAndClasses/MyVisitsThisYear";
+import { getMe } from "../../services/userAPI";
+import Spinner from "../../components/Spinner";
 
 function MyTickets() {
+  const { data, isPending } = useQuery({
+    queryKey: ["me"],
+    queryFn: getMe,
+  });
+
+  if (isPending) return <Spinner />;
+
   return (
     <div className="my-16 flex flex-col gap-12">
       <Header />
@@ -18,7 +28,7 @@ function MyTickets() {
           <MyVisitsThisYear />
         </div>
         <div className="mt-20">
-          <WelcomeSection ticketsPage />
+          <WelcomeSection ticketsPage data={data} />
         </div>
       </div>
     </div>
