951c6f260c451c0cf6aaa948d8e3a8a23f4e9628
nikovolaric <niko.volaric@gmail.com>
2025-06-28 19:15:57 +0200

    Initial commit

--- src/features/auth/components/Logout.tsx
+++ src/features/auth/components/Logout.tsx
@@ -1,38 +1,44 @@
 import { ChevronLeftIcon } from "@heroicons/react/24/outline";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { logout } from "../../../services/authAPI";
-import { Navigate } from "react-router";
-import { useMutation } from "@tanstack/react-query";
-import Spinner from "../../../components/Spinner";
+import { useNavigate } from "react-router";
 
 function Logout() {
-  const { mutate, isPending, data } = useMutation({
-    mutationFn: logout,
-  });
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
+    try {
+      e.preventDefault();
+      setIsLoading(true);
+      const data = await logout();
 
-    mutate();
-  }
-
-  if (isPending) {
-    return <Spinner />;
-  }
-
-  if (data && data.status === "success") {
-    setTimeout(function () {
-      return <Navigate to="/" replace />;
-    }, 1000);
+      if (data.status === "success") {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <button className="hover:bg-secondary flex cursor-pointer items-center gap-2 rounded-lg border border-black px-3 py-2.5 font-semibold transition-colors duration-300 hover:border-black/0">
-        <span>
-          <ChevronLeftIcon className="h-4 stroke-3" />
-        </span>
-        Odjavi se
+      <button
+        className="hover:bg-secondary flex cursor-pointer items-center gap-2 rounded-lg border border-black px-3 py-2.5 font-semibold transition-colors duration-300 hover:border-black/0"
+        disabled={isLoading}
+      >
+        {isLoading ? (
+          "..."
+        ) : (
+          <>
+            <span>
+              <ChevronLeftIcon className="h-4 stroke-3" />
+            </span>
+            Odjavi se
+          </>
+        )}
       </button>
     </form>
   );
